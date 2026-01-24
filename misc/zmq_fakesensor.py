ec8cd63fe793051053ff612d9f4216218a777bfb
Max Lawton <max@lawtonnation.com>
2026-01-24 17:53:56 -0500

    new json

--- misc/zmq_fakesensor.py
+++ misc/zmq_fakesensor.py
@@ -8,7 +8,7 @@ def main() -> None:
     socket: zmq.Socket = context.socket(zmq.PUB)
     socket.bind("ipc:///tmp/cyphal_out")
     while True:
-        message = "{" + f"'subject_id': {random.randint(0, 1000)}" + f"'value': {random.randint(1, 10000) / 100.0}" + "}"
+        message = "{" + f"'subject_id': {random.randint(0, 1000)}, " + f"'value': {random.randint(1, 10000) / 100.0}" + "}"
         socket.send_string(message)
         time.sleep(.1)
 
