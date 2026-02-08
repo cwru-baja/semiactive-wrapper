93c1e25ea9df08eb36338b9a499b9fc6e97b7683
Max Lawton <max@lawtonnation.com>
2026-02-08 18:50:07 -0500

    new zmq push

--- misc/zmq_fakesensor.py
+++ misc/zmq_fakesensor.py
@@ -5,7 +5,7 @@ import time
 
 def main() -> None:
     i = 1
-    
+
     msgs = [
         "{'subject_id': 578, 'type':'uavcan.primitive.String_1_0', 'value': 'sup dood'}",
         "{'subject_id': 908, 'type':'cwrubaja.suspension.vcm.Setpoint_0_2', 'priority': 2, 'current': {'ampere': 0.4}}"
@@ -15,7 +15,7 @@ def main() -> None:
     socket: zmq.Socket = context.socket(zmq.PUB)
     socket.bind("ipc:///tmp/cyphal_out")
     while True:
-        message = msgs[random.randint(1, 2)]
+        message = msgs[random.randint(0, 1)]
         socket.send_string(message)
         print(f"\rZMQ Sensor Outputs: {i}", end="", flush=True)
         i+=1
