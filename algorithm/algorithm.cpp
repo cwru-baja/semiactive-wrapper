5c14a62dad843621d2c7812ccb3904c4cb5ea6fe
Max Lawton <91839824+max-lawton@users.noreply.github.com>
2026-04-27 20:26:34 -0400

    Update algorithm

--- algorithm/algorithm.cpp
+++ algorithm/algorithm.cpp
@@ -9,8 +9,9 @@
  ** Algorithm File **
 ===============================================================================
 
+    Name    : Test Algorithm
     Version : 1.0
-    Date    : 24 April 2026
+    Date    : 27 April 2026
     Author  : Max Lawton
 
     Purpose:
@@ -31,10 +32,24 @@ void setup(int subject_id, SharedAlgorithmMemory& m, ZMQSensorData& s, ZMQOutput
 // update
 // -----------------------------------------------------------------------------
 void update(int subject_id, SharedAlgorithmMemory& m, ZMQSensorData& s, ZMQOutput& o) {
-    
-        usleep(1000); // Simulate algorithm runtime
 
-        o.setSetpoint(subject_id, s.engine_output_rotation_sensor.getData<double>("radian_per_second")*1e10);
+        // get the brake pressure
+        bool is_front = (subject_id == 3100 || subject_id == 3101);
+        double brake_pressure = 0.0;
+        if (is_front) {
+            brake_pressure = s.front_brake_pressure_sensor.getData<double>("pascal");
+        } else {
+            brake_pressure = s.rear_brake_pressure_sensor.getData<double>("pascal");
+        }
+
+        // get setpoint based on engine rpm and brake pressure
+        double setpoint = s.engine_output_rotation_sensor.getData<double>("radian_per_second")*1e10 + brake_pressure;
+
+        // 1.8 amps max
+        if      (setpoint < 0.0) setpoint = 0.0;
+        else if (setpoint > 1.8) setpoint = 1.8;
+
+        o.setSetpoint(subject_id, setpoint);
 }
 
 // -----------------------------------------------------------------------------
